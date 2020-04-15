edf187c9eac20946d3d623f07c9d2b6e32589bdc
Milesq <wmilosz88@gmail.com>
2020-04-16 00:35:30 +0200

    Add basic site structure

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,9 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div>
+    <div>
       <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
-
-<script>
-export default {
-  mounted() {
-    console.log(true);
-  },
-};
-</script>
-
-<style lang="scss">
-#app {
-  color: #2c3e89;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-}
-
-.nav {
-  padding: 30px;
-
-  .a {
-    color: #2c3e50;
-    font-weight: bold;
-
-    &.router-link-exact-active {
-      color: #42b983;
-
-      b {
-        color: white;
-
-        a {
-          color: red;
-        }
-      }
-    }
-  }
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Board from '../views/Board.vue';
 
 Vue.use(VueRouter);
 
@@ -11,12 +12,14 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/board',
+    name: 'Board',
+    component: Board,
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import(/* webpackChunkName: "admin" */ '../views/Admin.vue'),
   },
 ];
 
