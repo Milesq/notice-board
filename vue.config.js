f847953ab03d790743e1130d3fa2741b9cc640d2
Milesq <wmilosz88@gmail.com>
2020-04-15 13:52:29 +0200

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+const stylelintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  publicPath: './',
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [new stylelintPlugin({ files: ['src/**/*.{vue,scss}'], emitWarning: true })],
+  },
+};
