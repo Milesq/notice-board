8c4555d6bb0cfd45e2d3d23e063b69c87789b99d
Milesq <wmilosz88@gmail.com>
2020-04-16 11:25:41 +0200

    Remove buefy

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
 import Vue from 'vue';
-import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
-
-Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
