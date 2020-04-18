0f288317ab41ef1102b9fd7c4ce692b6b2375d3e
Milesq <wmilosz88@gmail.com>
2020-04-18 22:19:33 +0200

    Add toggle theme button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <v-app>
     <v-app-bar dark app color="primary">
-      <v-toolbar-title>
-        <v-btn text rounded to="/">{{ $t('title') }}</v-btn>
-      </v-toolbar-title>
+      <!-- <v-toolbar-title> -->
+      <v-btn text rounded to="/">{{ $t('title') }}</v-btn>
+      <v-spacer />
+      <v-btn @click="changeTheme" text fab dark>
+        <v-icon>mdi-brightness-6</v-icon>
+      </v-btn>
+      <!-- </v-toolbar-title> -->
     </v-app-bar>
 
     <v-content>
@@ -11,3 +15,14 @@
     </v-content>
   </v-app>
 </template>
+
+<script>
+export default {
+  methods: {
+    changeTheme() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+      localStorage.setItem('darkTheme', this.$vuetify.theme.dark);
+    },
+  },
+};
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,13 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import light from '../colors.json';
+import colors from '../colors.json';
 
 Vue.use(Vuetify);
 
+const { dark, light } = colors;
+
 export default new Vuetify({
   theme: {
+    dark: localStorage.getItem('darkTheme') === 'true',
     themes: {
       light,
+      dark,
     },
   },
 });
