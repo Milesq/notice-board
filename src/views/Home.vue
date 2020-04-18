07117935667a2392bcbe9523116562586bddf7cb
Milesq <wmilosz88@gmail.com>
2020-04-18 14:29:01 +0200

    Add login form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,32 @@
 <template>
-  <div>Zaloguj się</div>
+  <v-container>
+    <v-row justify="center">
+      <v-col sm="8" md="4">
+        <v-card>
+          <v-card-title>Podaj imię i nazwisko</v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="submit">
+              <v-text-field v-model="name" label="Imie i nazwisko" />
+              <v-divider />
+              <v-card-actions>
+                <v-spacer />
+                <v-btn type="submit" color="primary">ok</v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    name: '',
+  }),
+  methods: {
+    submit() {},
+  },
+};
+</script>
