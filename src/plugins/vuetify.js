6278a5ece2435bd383e370d1366c98c0b9b37b2a
Milesq <wmilosz88@gmail.com>
2020-04-18 15:43:15 +0200

    Customize theme 🎉

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,13 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import light from '../colors.json';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light,
+    },
+  },
+});
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const stylelintPlugin = require('stylelint-webpack-plugin');
+const { primary: themeColor } = require('./src/colors.json');
 
 module.exports = {
   publicPath: './',
@@ -7,4 +8,8 @@ module.exports = {
     plugins: [new stylelintPlugin({ files: ['src/**/*.{vue,scss}'], emitWarning: true })],
   },
   transpileDependencies: ['vuetify'],
+  pwa: {
+    name: 'Announcements',
+    themeColor,
+  },
 };
