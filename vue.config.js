8690a026a8409fd47856547bc057479220579851
Milesq <wmilosz88@gmail.com>
2020-04-19 13:54:41 +0200

    Remove public path

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const stylelintPlugin = require('stylelint-webpack-plugin');
 const { primary: themeColor } = require('./src/colors.json');
 
 module.exports = {
-  publicPath: './',
   productionSourceMap: false,
   configureWebpack: {
     plugins: [
