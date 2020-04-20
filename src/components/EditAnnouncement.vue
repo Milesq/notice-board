2a18fcd8721bc583dceb5a664a49eb45eb656508
Milesq <wmilosz88@gmail.com>
2020-04-21 00:14:35 +0200

    Add basic content editor

--- src/components/EditAnnouncement.vue
+++ src/components/EditAnnouncement.vue
@@ -16,11 +16,33 @@
           <v-btn dark text @click="dialog = false">Save</v-btn>
         </v-toolbar-items>
       </v-toolbar>
-      <v-container width="50%" class="py-5 px-sm-10">
+      <v-container class="py-5 px-sm-10">
         <v-row>
-          <v-col cols="12" sm="6" class="mx-auto">
+          <v-col cols="12" sm="10" class="mx-auto">
             <v-form>
-              <v-text-field width="50%" :label="$t('titleWord')" outlined />
+              <v-text-field
+                v-model="title"
+                style="max-width: 30%"
+                :label="$t('titleWord')"
+                outlined
+              />
+              <editor
+                api-key="wfnyipsdja07y0q2ttktp68jkxvn5b8eqd0egr7yk65qdj0y"
+                v-model="content"
+                :init="{
+                  height: 500,
+                  menubar: true,
+                  plugins: [
+                    'advlist autolink lists link image charmap print preview anchor',
+                    'searchreplace visualblocks code fullscreen',
+                    'insertdatetime media table paste code help wordcount',
+                  ],
+                  toolbar:
+                    'undo redo | formatselect | bold italic backcolor | \
+                    alignleft aligncenter alignright alignjustify | \
+                    bullist numlist outdent indent | removeformat | help',
+                }"
+              />
             </v-form>
           </v-col>
         </v-row>
@@ -30,9 +52,29 @@
 </template>
 
 <script>
+import Editor from '@tinymce/tinymce-vue';
+
 export default {
   data: () => ({
     dialog: false,
+    title: '',
+    content: '',
   }),
+  props: {
+    value: {
+      type: Object,
+      default: () => ({
+        title: '',
+        content: '',
+      }),
+    },
+  },
+  mounted() {
+    this.title = this.value.title;
+    this.content = this.value.content;
+  },
+  components: {
+    Editor,
+  },
 };
 </script>
