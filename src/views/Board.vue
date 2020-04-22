7bb0f81db5e04c48e48689f80b00a12256c112dd
Milesq <wmilosz88@gmail.com>
2020-04-22 12:17:33 +0200

    Fix announcements on dark mode, add possible tile systems

--- src/views/Board.vue
+++ src/views/Board.vue
@@ -7,13 +7,18 @@
         <v-col
           v-for="(item, i) in announcements"
           :key="`announcement-${i}`"
-          :cols="cols(item.title.length)"
+          cols="12"
+          :sm="cols(item.title.length)"
         >
           <v-hover v-slot="{ hover }">
             <v-slide-y-transition>
-              <v-card min-height="150" :elevation="hover ? 8 : 2">
+              <v-card
+                min-height="150"
+                :class="['grey', theme.isDark ? 'darken-3' : 'lighten-4']"
+                :elevation="hover ? 4 : 1"
+              >
                 <v-card-title>{{ item.title }}</v-card-title>
-                <v-card-subtitle v-html="item.content"></v-card-subtitle>
+                <v-card-text v-html="item.content" />
               </v-card>
             </v-slide-y-transition>
           </v-hover>
@@ -25,13 +30,13 @@
 
 <script>
 // prettier-ignore
-const possibilities = [
+let possibilities = [
   [3, 3, 3, 3],
   [4, 4, 4],
   [6, 6],
   [8, 4],
   [4, 8],
-  [12]
+  [12],
 ];
 
 let fillingSystem = [];
@@ -40,15 +45,26 @@ export default {
   data: () => ({
     announcements: [],
   }),
+  inject: ['theme'],
   created() {
+    fillingSystem = [];
     this.$announcements.get().then(({ docs }) => {
       this.announcements = docs.map(item => ({ id: item.id, ...item.data() }));
     });
+
+    if (this.$vuetify.breakpoint.sm) {
+      // prettier-ignore
+      possibilities = [
+        [8, 4],
+        [12],
+        [6, 6],
+      ];
+    }
   },
   methods: {
     cols(x) {
       if (fillingSystem.length === 0) {
-        fillingSystem = possibilities[x % possibilities.length];
+        fillingSystem = Array.from(possibilities[x % possibilities.length]);
       }
 
       return fillingSystem.pop();
