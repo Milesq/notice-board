795ab50af03b445aa68ae7162418809d2ea6cb69
Milesq <wmilosz88@gmail.com>
2020-06-27 16:11:48 +0200

    Remove unsued informations from firebase messaging sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,23 +2,15 @@
 /* global firebase */
 
 // [START initialize_firebase_in_sw]
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/7.14.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.14.1/firebase-messaging.js');
-// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+
 firebase.initializeApp({
-  apiKey: 'AIzaSyCSvOMNR7nrCa548aXG9Jb87G3z_DatT9A',
-  authDomain: 'tests-d8957.firebaseapp.com',
-  databaseURL: 'https://tests-d8957.firebaseio.com',
+  apiKey: 'process.env.VUE_APP_apiKey',
   projectId: 'tests-d8957',
-  storageBucket: 'tests-d8957.appspot.com',
   messagingSenderId: '305225525597',
   appId: '1:305225525597:web:434ee2e0f5e8b5b6fca0b4',
-  measurementId: 'G-8R4SBW6S2K',
 });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
 firebase.messaging();
 // [END initialize_firebase_in_sw]
