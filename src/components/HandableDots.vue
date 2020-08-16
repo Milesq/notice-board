5dacd194b4162be81cdff0880d587358e5cf8373
Milesq <wmilosz88@gmail.com>
2020-08-16 13:14:06 +0200

    Add dots background dependent from background

--- src/components/HandableDots.vue
+++ src/components/HandableDots.vue
@@ -1,9 +1,17 @@
 <template>
-  <div></div>
+  <div class="dots">
+    <span
+      class="dots__element"
+      :style="{ backgroundColor: theme.isDark ? color.dark : color.light }"
+      v-for="i in n"
+      :key="i"
+    ></span>
+  </div>
 </template>
 
 <script>
 export default {
+  inject: ['theme'],
   props: {
     n: {
       type: Number,
@@ -13,6 +21,12 @@ export default {
       },
     },
   },
+  data: () => ({
+    color: {
+      dark: 'rgba(0, 0, 0, .57)',
+      light: 'rgba(255, 255, 255, .87)',
+    },
+  }),
 };
 </script>
 
