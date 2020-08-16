dffc366c71309955660363338ff5b9f624d0b029
Milesq <wmilosz88@gmail.com>
2020-08-16 12:47:12 +0200

    Add skeleton for HandableDots component

---
+++ src/components/HandableDots.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    n: {
+      type: Number,
+      default: 6,
+      validator(n) {
+        return n > 0;
+      },
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
