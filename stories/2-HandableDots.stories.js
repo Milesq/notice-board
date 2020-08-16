e404c0cd9f80a7313911c4d6c7b4e8a6afd914bf
Milesq <wmilosz88@gmail.com>
2020-08-16 12:51:08 +0200

    chore: Add HandableDots stories

---
+++ stories/2-HandableDots.stories.js
@@ -0,0 +1,19 @@
+import HandableDots from '../src/components/HandableDots.vue';
+
+export default {
+  title: 'HandableDots',
+};
+
+export const sixDots = () => ({
+  template: '<HandableDots />',
+  components: {
+    HandableDots,
+  },
+});
+
+export const nineDots = () => ({
+  template: '<HandableDots />',
+  components: {
+    HandableDots,
+  },
+});
